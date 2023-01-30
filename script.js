54ba51a3e04c273b3ed7daf2565f3ffdb5edadf1
Edu7NN <96152246+Edu7NN@users.noreply.github.com>
2023-01-30 09:25:18 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,93 @@
+
+
+
+//INTERAÇÃO
+const citySearchInput = document.getElementById('city-search-input');
+const citySearchButton = document.getElementById('city-search-button');
+
+//EXIBIÇÃO
+const currentDate = document.getElementById('current-date');
+const cityName = document.getElementById('city-name');
+const weatherIcon = document.getElementById('weather-icon');
+const weatherDescription = document.getElementById('weather-description');
+const currentTemperature = document.getElementById('current-temperature');
+const winSpedd = document.getElementById('wind-speed');
+const feelsLikeTemperature = document.getElementById('feels-like-temperature');
+const currentHumidity = document.getElementById('current-humidity');
+const sunriseTime = document.getElementById('sunrise-time');
+const sunsetTime = document.getElementById('sunset-time');
+
+const api_key = "0f2924124f779e8692b9c2885acc140c";
+
+citySearchButton.addEventListener( 'click', ()=> {
+
+  let cityName =  citySearchInput.value
+    getCityWeather(cityName);
+});
+
+navigator.geolocation.getCurrentPosition(
+    (position) => {
+        let lat = position.coords.latitude
+        let lon = position.coords.longitude
+
+        getCurrentLocationWeather(lat, lon);
+
+    },
+    (err) => {
+        if(err.code === 1) {
+            alert("Geolocalização negada pelo usuário, busque manualmente por uma cidade através da barra de pesquisa.");
+        } else {
+            console.log(err);
+        }
+    }
+)
+
+function getCurrentLocationWeather(lat, lon) {
+        fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=metric&lang=pt_br&appid=${api_key}`)
+        .then((response) => response.json())
+        .then((data) => displayWeather(data));
+}
+
+function getCityWeather(cityName) {
+
+    weatherIcon.src = `./assets/loading-icon.svg`;
+
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cityName}&units=metric&lang=pt_br&appid=${api_key}`)
+    .then((response) => response.json())
+    .then((data) => displayWeather(data))
+}
+
+function displayWeather(data) {
+   let { 
+    dt,
+    name,
+    weather: [{ icon, description }],
+    main: { temp, feels_like, humidity },
+    wind: { speed },
+    sys: { sunrise, sunset },
+    } = data
+
+    currentDate.textContent = formatDate(dt);
+    cityName.textContent = name;
+    weatherIcon.src = `./assets/${icon}.svg`;
+    weatherDescription.textContent = description;
+    currentTemperature.textContent = `${Math.round(temp)}°C`;
+    winSpedd.textContent = `${Math.round(speed * 3.6)}Km/h`;
+    feelsLikeTemperature.textContent = `${Math.round(feels_like)}°C`;
+    currentHumidity.textContent = `${humidity}%`;
+    sunriseTime.textContent = formatTime(sunrise);
+    sunsetTime.textContent = formatTime(sunset);
+}
+
+function formatDate(epochTime) {
+    let date = new Date(epochTime * 1000);
+    let formatDate = date.toLocaleDateString('pt-BR', { month: 'long', day: 'numeric'});
+    return `Hoje, ${formatDate}`;
+}
+
+function formatTime(epochTime) {
+    let date = new Date(epochTime * 1000);
+    let hours = date.getHours();
+    let minutes = date.getMinutes();
+    return `${hours}:${minutes}`;
+}
